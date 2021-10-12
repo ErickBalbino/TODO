76d5489b8b10655e372548f285741b0c442cc060
Erick Balbino da Silva <erickbaddog2@gmail.com>
2021-10-12 12:34:12 -0300

    update project
    subindo arquivos iniciais

---
+++ tasks/views.py
@@ -0,0 +1,10 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def helloWorld(request):
+    return HttpResponse("Hello World")
+
+def taskList(request):
+    return render(request, 'list.html')
